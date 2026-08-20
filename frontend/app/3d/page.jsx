f94ad18102f32a355cad6a6a8f12a33dc412cf55
saimishfaqmirza <saimishfaq.code@gmail.com>
2026-08-20 11:49:51 +0500

    feat: add interactive 3D experience

---
+++ frontend/app/3d/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ProductScene from "../../components/3d/ProductScene";
+
+export default function ThreeDPage() {
+  return (
+    <main style={{ width: "100%", height: "100vh" }}>
+      <ProductScene />
+    </main>
+  );
+}
